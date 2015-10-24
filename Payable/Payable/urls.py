8d929f852c42d3f45ef7651b2f3c3e443e4fbc67
Arjun Bhatnagar <me@arjunb.com>
2015-10-24 12:55:57 -0700

    added urls

--- Payable/Payable/urls.py
+++ Payable/Payable/urls.py
@@ -18,4 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^home/$', 'PayableCore.views.home'),
+    url(r'^landing/$', 'PayableCore.views.dashboard'),
+    url(r'^register/$', 'PayableCore.views.register'),
 ]
