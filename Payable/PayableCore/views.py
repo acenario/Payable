3e3be852ed4c8a3f2b804e3898ed5bf9027a5308
Dakota Nelson <Dakota.Nelson@students.olin.edu>
2015-10-24 16:05:08 -0400

    remote .pyc files, create template and view placeholder

--- Payable/PayableCore/views.py
+++ Payable/PayableCore/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def dashboard(request):
+    return render(request, 'dashboard.html', context)
+
+def index(request):
+    return render(request, 'index.html', context)
+
+def register(request):
+    return render(request, 'register.html', context)
